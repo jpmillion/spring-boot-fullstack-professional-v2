04ad8680473798ed2debdf946faddf85cf796a8a
jpmillion <jpmillion28@gmail.com>
2021-10-18 08:58:31 -0400

    Initial commit

---
+++ src/main/java/com/million/amigos/student/Gender.java
@@ -0,0 +1,7 @@
+package com.million.amigos.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/million/amigos/student/Student.java
@@ -0,0 +1,17 @@
+package com.million.amigos.student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
