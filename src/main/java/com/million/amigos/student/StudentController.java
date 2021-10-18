ab36c240df7a1992ad106dd5f78f5e6f2f0d0112
jpmillion <jpmillion28@gmail.com>
2021-10-18 09:42:40 -0400

    Define getAllStudents in StudentController

--- src/main/java/com/million/amigos/student/StudentController.java
+++ src/main/java/com/million/amigos/student/StudentController.java
@@ -1,2 +1,25 @@
-package com.million.amigos.student;public class StudentController {
+package com.million.amigos.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return Arrays.asList(
+                new Student(
+                        1L,
+                        "John",
+                        "Million",
+                        Gender.MALE
+                )
+        );
+    }
 }
